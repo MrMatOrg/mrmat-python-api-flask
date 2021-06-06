3a2f786235ce99b4f7d836af57e6670085008ed1
Mathieu Imfeld <imfeldma@gmail.com>
2021-06-06 16:25:20 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mrmat-python-api-flask',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     license='MIT',
     author='MrMat',
