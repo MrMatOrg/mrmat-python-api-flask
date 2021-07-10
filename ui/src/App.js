4304715bc351c1f5a989438a4a0eff205c9cb228
MrMat <imfeldma@gmail.com>
2021-07-10 08:31:18 +0200

    Invoking the greetingv1 api

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,20 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [currentGreetingV1, setCurrentGreetingV1] = useState('Unknown');
+
+  useEffect(() => {
+    fetch('/api/greeting/v1/').then(res => res.json()).then(data => {
+      setCurrentGreetingV1(data.message);
+    })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>The current greeting is {currentGreetingV1}</p>
     </div>
   );
 }
