dc07ae05d79cfc9d5639089693fe7d7a6abc7898
MrMat <imfeldma@gmail.com>
2021-07-31 11:27:12 +0200

    Introduced react API

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {AuthenticationProvider, oidcLog, withOidcSecure} from '@axa-fr/react-oidc-context';
+
+import { Menu, Segment } from 'semantic-ui-react';
+
+
+import oidcConfiguration from './configuration';
+
+import Home from "./Home";
+import About from "./About";
+import Error from "./Error";
+import UserInfo from "./UserInfo";
+
 import './App.css';
 
 function App() {
+    let state = { activeItem: 'home' };
 
-  const [currentGreetingV1, setCurrentGreetingV1] = useState('Unknown');
+    return (
+        <Router>
+            <AuthenticationProvider configuration={oidcConfiguration} loggerLevel={oidcLog.DEBUG}>
 
-  useEffect(() => {
-    fetch('/api/greeting/v1/').then(res => res.json()).then(data => {
-      setCurrentGreetingV1(data.message);
-    })
-  })
+                <div>
+                    <Menu pointing secondary>
+                        <Menu.Item name='home' as={Link} to='/'>Home</Menu.Item>
+                        <Menu.Item name='about' as={Link} to='/about'>About</Menu.Item>
+                        <Menu.Item name='secure' as={Link} to='/secure'>Secure</Menu.Item>
+                        <Menu.Menu position='right'>
+                            <Menu.Item name='logout' as={Link}>Logout</Menu.Item>
+                        </Menu.Menu>
+                    </Menu>
+                </div>
 
-  return (
-    <div className="App">
-      <p>The current greeting is {currentGreetingV1}</p>
-    </div>
-  );
+                <Segment basic>
+                    <Switch>
+                        <Route exact path="/"><Home/></Route>
+                        <Route path="/about"><About/></Route>
+                        <Route path="/secure" component={withOidcSecure(UserInfo)}/>
+                        <Route path="*"><Error/></Route>
+                    </Switch>
+                </Segment>
+            </AuthenticationProvider>
+        </Router>
+    );
 }
+
 
 export default App;
